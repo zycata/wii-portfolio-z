5e6ea4cb7a3b2fdf07e48f36e6960411eb8fd81e
Zyc <166348388+zycata@users.noreply.github.com>
2026-03-08 23:56:17 -0700

    github pages moment

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
         <StrictMode>
             <App />
         </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/wii-portfolio-z",
   plugins: [react()],
   css: {
     postcss: {
